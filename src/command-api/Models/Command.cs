6a491427f712991469836e46a09d5eb135bd17c4
Kien Nguyen The <cudayanh@gmail.com>
2021-08-09 14:59:48 +0900

    add Command model to src/command-api

---
+++ src/command-api/Models/Command.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace command-api.Models
+{
+    public class Command 
+    {
+        [Required]
+        public int Id{ get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
